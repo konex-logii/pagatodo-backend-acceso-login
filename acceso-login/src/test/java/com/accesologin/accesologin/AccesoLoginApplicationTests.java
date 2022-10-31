7c99b6c1051d0f51bdccf54998d07ba875f18f69
manuelriverak <manuel.rivera@konexinnovation.com.co>
2022-10-31 10:35:37 -0500

    commit inicial

---
+++ acceso-login/src/test/java/com/accesologin/accesologin/AccesoLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accesologin.accesologin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesoLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
